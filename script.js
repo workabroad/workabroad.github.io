c4019fe74a35472e767c4beb9ad4d0c392ec87a7
workabroad <46955891+workabroad@users.noreply.github.com>
2019-02-01 23:46:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function(){
+
+  $('.photos1').slick({
+    arrows: false,
+    loop: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+
+  $('.photos2').slick({
+    arrows: false,
+    loop: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+
+});
