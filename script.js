ba5ccc2793cf93f5b312e97454333fafde1b23a6
workabroad <46955891+workabroad@users.noreply.github.com>
2019-02-02 16:56:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 $(function(){
+  var $items1 = $('.photos1 img');
+  var $items2 = $('.photos2 img');
 
-  $('.photos1').slick({
-    arrows: false,
-    loop: true,
-    autoplay: true,
-    autoplaySpeed: 3000
-  });
+  $('.photos1').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        }
+    },
+    onInitialized: function(){
+      equalHeight($items1);
+    }
+});
 
-  $('.photos2').slick({
-    arrows: false,
-    loop: true,
-    autoplay: true,
-    autoplaySpeed: 3000
-  });
+$('.photos2').owlCarousel({
+  loop:true,
+  margin:10,
+  responsive:{
+      0:{
+          items:1
+      }
+  },
+  onInitialized: function(){
+    equalHeight($items2);
+  }
+});
+
+  function equalHeight($items){
+    var max = 0;
+
+    $items.each(function(){
+      var h = $(this).height();
+      console.log(h);
+      if(h > max){
+        max = h;
+      }
+    });
+    console.log(max);
+    $items.css('height', max + 'px');
+  }
+
+
 
 });
